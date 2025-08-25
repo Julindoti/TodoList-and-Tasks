d7de24772ba9099c1f7b91598261311e54a89976
julindoti <julinmestredoti@gmail.com>
2025-08-25 16:07:11 -0300

    fixed:localStorage didnt save items properly

--- Todo/script.js
+++ Todo/script.js
@@ -1,32 +1,29 @@
 // TODO LIST
 
-const unfinished = document.getElementById("unfinished")
 //const taskUnfinished = document.querySelectorAll('.task');
-const taksCompleted = document.getElementById("completed")
+const unfinished = document.getElementById("unfinished");
+const taskCompleted = document.getElementById("completed");
 
+function handleTodo(){
 
-function handleTodo(){  
-  console.log("KRATOS")
+  console.log("KRATOS");
+  const todoInput = document.getElementById("todo");
+  const todo = todoInput.value.trim();
 
-  const todoInput = document.getElementById("todo")
-  const todo = todoInput.value.trim()
   if(!todo) return;
   
   const savedList = JSON.parse(localStorage.getItem("Tasklist")) || [];
   savedList.push(todo);
   localStorage.setItem("Tasklist", JSON.stringify(savedList));
+  console.log(savedList);
   
   const newDiv = document.createElement('div');
-  newDiv.className = "task"
-  newDiv.textContent = todo
-  
+  newDiv.className = "task";
+  newDiv.textContent = todo;
   newDiv.addEventListener("click", function () {
-      taksCompleted.appendChild(newDiv);
+      taskCompleted.appendChild(newDiv);
       }) 
-      
-      unfinished.appendChild(newDiv)
-      
-      todoInput.value = "";
   
-
+      unfinished.appendChild(newDiv);     
+      todoInput.value = "";
 }
