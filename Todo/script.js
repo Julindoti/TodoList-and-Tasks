3540d22d9a6a8684645db102b340d6f7ad35a0f9
julindoti <julinmestredoti@gmail.com>
2025-08-26 00:53:44 -0300

    add:clear btn for unfinshed tasks & a little of css stying

--- Todo/script.js
+++ Todo/script.js
@@ -1,10 +1,28 @@
 // TODO LIST
 
 //const taskUnfinished = document.querySelectorAll('.task');
-const unfinished = document.getElementById("unfinished");
+const taskUnfinished = document.getElementById("unfinished");
 const taskCompleted = document.getElementById("completed");
+const todoCard = document.getElementById("todo_card");
+const title =  taskUnfinished.children[0];  
 
 //localStorage.clear() => Use this method up here to reset the localStorage Adriano!
+todoCard.addEventListener("click", function(){
+      document.body.style.
+    
+})
+
+document.addEventListener('keydown', (event)=>{
+     if(event.key === "Enter"){ handleTodo() }
+     if(event.key === "Delete"){ clearStorage() }
+})
+
+function clearStorage(){
+
+     localStorage.clear();
+     taskUnfinished.replaceChildren();
+     taskUnfinished.appendChild(title);
+}
 
 function handleTodo(){
 
