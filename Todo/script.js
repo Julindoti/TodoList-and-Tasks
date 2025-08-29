8f0bf0bea8d62c116396cb13d7270d0ccb42e6ee
julindoti <julinmestredoti@gmail.com>
2025-08-29 18:00:23 -0300

    fixed: js logic and html showing items unproperly

--- Todo/script.js
+++ Todo/script.js
@@ -1,5 +1,4 @@
 // TODO LIST
-localStorage.clear()
 //const taskUnfinished = document.querySelectorAll('.task');
 const taskUnfinished = document.getElementById("unfinished");
 const taskCompleted = document.getElementById("completed");
@@ -15,9 +14,7 @@ let array_list =  {state: "unfinished" , text :''}
 
 //localStorage.clear() //Use this method up here to reset the localStorage Adriano!
 
-
 deleteZone.addEventListener("dragover", mouseDown())
-
 
 input.addEventListener("input", function(){
      this.style.height="auto";
@@ -27,15 +24,9 @@ document.addEventListener('keydown', (event)=>{
      if(event.key === "Enter"){ handleTodo() }
      if(event.key === "Delete"){ input.value=""}
 })   
-
-
 function mouseDown(){
-     console.log("Atreus")
-     
      deleteZone.addEventListener("dragleave", ()=>{clearStorage()})
-     
 }
-
 function clearStorage(){
      
      localStorage.clear();
@@ -47,13 +38,16 @@ function clearStorage(){
      taskCompleted.appendChild(hrC);
      
 }
+function deleteTask(){
 
+
+}
 function handleTodo(){
      
      console.log("KRATOS");
      const todoInput = document.getElementById("todo");
      const todo =  todoInput.value.trim();
-
+     if(!todo) return;
      const list_item= array_list =  {state:"unfinished", text: `${todo}`};
      console.log(array_list)
      console.log(list_item.state)
@@ -62,78 +56,51 @@ function handleTodo(){
      savedList.push(array_list);
      localStorage.setItem("Tasklist", JSON.stringify(savedList));
      console.log(savedList);
-  
-
      createDiv(todo, list_item)
-  
-
-  // setTimeout(()=>{
-
-       //     localLoad(savedList, taskCompleted)
-
-     // },2000)
- 
-
 }
 
 function localLoad(savedList){
-     
+
      if(savedList){
           savedList.forEach(items => {
-               if (items.state === "unfinished"){
-                    
-                    console.log("não salvo")
-
-            }else if (items.state === "completed") {
-
-               console.log("salvo")
-            }
-         
-              console.log(items)
-              createDiv(items.text)
-
+              if (items.state === "unfinished"){
+                    createDiv(items.text, items.state)
+              }else if (items.state === "completed") {
+                    createDiv(items.text, items.state)
+             } 
          })
-
      }
-
 }
 function createDiv(text, state){
     
-      
      const newDiv = document.createElement('div');
      newDiv.className = "task";
      newDiv.textContent = text;
      newDiv.draggable= "true";
-     completedTask(newDiv,text, state)
-     
-         unfinished.appendChild(newDiv);     
+     completedTask(newDiv,text)
+        if(state === "completed"){
+          taskCompleted.appendChild(newDiv)
+        }else{
+         unfinished.appendChild(newDiv); }    
          input.value = "";
-        
-
+     return newDiv;
 } 
-function completedTask(div, text ,state){
-
-
+function completedTask(div, text){
+      
        div.addEventListener("click", function () {
-         changeState(text, "completed")
-         localStorage.setItem("Tasklist", JSON.stringify(state))
+         changeState(text)
          taskCompleted.appendChild(div);
-         console.log(array_list)
          completedAnimation();
-         }) 
-     
+         })   
 }
 function changeState(textTofind){
-
-let localgetitem = localGet;
-
-const items = localgetitem.findIndex( item => item.text === textTofind)
-
-if (items !== 1){
-     localgetitem[items].state = newState;
-     localStorage.setItem("Tasklist", JSON.stringify(localgetitem))
-
-}
+ 
+     const array_items = JSON.parse(localStorage.getItem("Tasklist") || [])
+     const indexed_item = array_items.findIndex( item => item.text === textTofind)
+     if (indexed_item !== -1){
+          array_items[indexed_item].state = "completed";
+          localStorage.setItem("Tasklist", JSON.stringify(array_items));
+     }
 }
 function completedAnimation(){
      
